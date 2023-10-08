e7545259c0ad4b95a07b89d6f0003cd00f33d751
mac <mac@macs-MacBook-Pro.local>
2023-10-08 16:47:06 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
 import logo from './logo.svg';
+import React from 'react';
+import ChatBot from 'react-simple-chatbot'
+import {Segment} from "semantic-ui-react"
 import './App.css';
 
 function App() {
+  
+  const steps = [
+    {
+      id:'Greet',
+      message:'hello welcome to student info system!',
+      user:false,
+      trigger:'ask name'
+    },
+    {
+      id:"ask name",
+      options:[
+        {value: "Got it", label:"Got it", trigger:"cat"},
+        // {value: "exit", label:"exit", trigger:"cat"}
+      ]
+    },
+    // {
+    //   id:'cat',
+    //   message:'waiting..',
+    //   trigger:'bat'
+    // },
+    {
+      id:'cat',
+      message:'what is your name',
+      user:false,
+      
+
+      trigger:'rat'
+    },
+    {
+      id:"rat",
+      user:true,
+      trigger:"cat4"
+    },
+    {
+      id:"cat4",
+      message:"what is your age",
+      user:false,
+      trigger:"ball"
+    },
+    {
+      id:"ball",
+      options:[
+        {value: "18", label:"18", trigger:"cat2"},
+        {value: "19", label:"19", trigger:"cat2"},
+        {value: "20", label:"20", trigger:"cat2"},
+        {value: "21", label:"21", trigger:"cat2"},
+        {value: "22", label:"22", trigger:"cat2"},
+        // {value: "exit", label:"exit", trigger:"cat"}
+      ]
+    },
+    
+    {
+      id:"cat2",
+      message:"Thank you in 5 seconds bot will exit",
+  
+      end:true,
+    },
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+      <Segment >
+        <ChatBot steps={steps}/>
+
+      </Segment>
+    </>
   );
 }
 
